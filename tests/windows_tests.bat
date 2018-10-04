34e0ffdb381751ff8c62a1a676af887e6b76f891
David Rowland <drowland96@gmail.com>
2018-10-04 11:21:38 +0100

    Disabled building VST2 formats for testing as these are supported by JUCE anymore

--- tests/windows_tests.bat
+++ tests/windows_tests.bat
@@ -54,6 +54,7 @@ exit /B %ERRORLEVEL%
     call "%PROJUCER_EXE%" --create-project-from-pip "%PLUGINS_PIP_DIR%\%PLUGIN_PIP_FILE%" "%TEMP_DIR%" "%ROOT%/modules/juce/modules"
     call "%PROJUCER_EXE%" --resave "%TEMP_DIR%/%PLUGIN_NAME%/%PLUGIN_NAME%.jucer"
     cd "%TEMP_DIR%/%PLUGIN_NAME%/Builds/VisualStudio2017"
+    set CL=/DJUCE_PLUGINHOST_VST#0
     "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" %PLUGIN_NAME%.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64  /p:TreatWarningsAsErrors=true
 
     :: Test out of process
