8a4d42f302b29a7655ac2b4244453732268894c2
David Rowland <drowland96@gmail.com>
2019-03-20 11:18:28 +0000

    Tests:   Enabled testing a simple juce plugin

--- tests/windows_tests.bat
+++ tests/windows_tests.bat
@@ -8,7 +8,8 @@ if not exist "%ROOT%" exit 1
 
 set PROJECT_NAME=pluginval
 set DEPLOYMENT_DIR=%ROOT%/bin/windows
-set PLUGINVAL_EXE=%DEPLOYMENT_DIR%\%PROJECT_NAME%.exe
+set PLUGINVAL_EXE=%ROOT%/Builds/VisualStudio2017/x64/Release/App/%PROJECT_NAME%.exe
+
 
 if not defined MSBUILD_EXE set MSBUILD_EXE=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe
 echo "MSBUILD_EXE: %MSBUILD_EXE%"
@@ -30,7 +31,7 @@ set TEMP_DIR=%ROOT%/tmp
 rd /S /Q "%TEMP_DIR%"
 
 cd "%PROJUCER_ROOT%"
-set CL=/DJUCER_ENABLE_GPL_MODE
+set CL=/DJUCER_ENABLE_GPL_MODE /GL
 "%MSBUILD_EXE%" Projucer.sln /p:VisualStudioVersion=15.0 /m /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64
 if not exist "%PROJUCER_EXE%" exit 1
 
@@ -40,14 +41,14 @@ if not exist "%PROJUCER_EXE%" exit 1
 ::============================================================
 :: Disabling these tests for now as they can't be built due to dependancies on the VST2 SDK which 
 :: can't be found without the use of Projucer's Global Settings
-rem call :TestPlugin "ArpeggiatorPlugin", "ArpeggiatorPluginDemo.h"
-rem call :TestPlugin "AudioPluginDemo", "AudioPluginDemo.h"
-rem call :TestPlugin "DSPModulePluginDemo", "DSPModulePluginDemo.h"
-rem call :TestPlugin "GainPlugin", "GainPluginDemo.h"
-rem call :TestPlugin "MultiOutSynthPlugin", "MultiOutSynthPluginDemo.h"
-rem call :TestPlugin "NoiseGatePlugin", "NoiseGatePluginDemo.h"
-rem call :TestPlugin "SamplerPlugin", "SamplerPluginDemo.h"
-rem call :TestPlugin "SurroundPlugin", "SurroundPluginDemo.h"
+:: call :TestPlugin "ArpeggiatorPlugin", "ArpeggiatorPluginDemo.h"
+call :TestPlugin "AudioPluginDemo", "AudioPluginDemo.h"
+:: call :TestPlugin "DSPModulePluginDemo", "DSPModulePluginDemo.h"
+:: call :TestPlugin "GainPlugin", "GainPluginDemo.h"
+:: call :TestPlugin "MultiOutSynthPlugin", "MultiOutSynthPluginDemo.h"
+:: call :TestPlugin "NoiseGatePlugin", "NoiseGatePluginDemo.h"
+:: call :TestPlugin "SamplerPlugin", "SamplerPluginDemo.h"
+:: call :TestPlugin "SurroundPlugin", "SurroundPluginDemo.h"
 exit /B %ERRORLEVEL%
 
 :TestPlugin
@@ -59,14 +60,15 @@ exit /B %ERRORLEVEL%
     call "%PROJUCER_EXE%" --create-project-from-pip "%PLUGINS_PIP_DIR%\%PLUGIN_PIP_FILE%" "%TEMP_DIR%" "%ROOT%/modules/juce/modules"
     call "%PROJUCER_EXE%" --resave "%TEMP_DIR%/%PLUGIN_NAME%/%PLUGIN_NAME%.jucer"
     cd "%TEMP_DIR%/%PLUGIN_NAME%/Builds/VisualStudio2017"
-    set CL=/DJUCE_PLUGINHOST_VST#0
+    set CL=/DJUCE_PLUGINHOST_VST#0 /GL
     "%MSBUILD_EXE%" %PLUGIN_NAME%.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64  /p:TreatWarningsAsErrors=true
+
+    :: Test in process
+    call "%PLUGINVAL_EXE%" --validate-in-process --strictness-level 5 --validate %PLUGIN_VST3%
+    if %ERRORLEVEL% NEQ 0 exit 1
 
     :: Test out of process
     call "%PLUGINVAL_EXE%" --strictness-level 5 --validate %PLUGIN_VST3%
     if %ERRORLEVEL% NEQ 0 exit 1
 
-    :: Test in process
-    call "%PLUGINVAL_EXE%" --validate-in-process --strictness-level 5 --validate %PLUGIN_VST3%
-    if %ERRORLEVEL% NEQ 0 exit 1
 exit /B 0
