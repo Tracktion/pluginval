fa5f648988bdc5f0e22a4c62d747609e7bc57ba5
reuk <reuk@users.noreply.github.com>
2021-06-04 13:35:16 +0100

    Fix formatting

--- Source/Validator.cpp
+++ Source/Validator.cpp
@@ -481,8 +481,8 @@ private:
             options.outputDir = File (v[IDs::outputDir].toString());
             options.withGUI = v.getProperty (IDs::withGUI, true);
             options.disabledTests = StringArray::fromLines (v.getProperty (IDs::disabledTests).toString());
-            options.sampleRates = juce::VariantConverter<std::vector<double>>::fromVar( v.getProperty (IDs::sampleRates) );
-            options.blockSizes  = juce::VariantConverter<std::vector<int>>::fromVar( v.getProperty (IDs::blockSizes) );
+            options.sampleRates = juce::VariantConverter<std::vector<double>>::fromVar (v.getProperty (IDs::sampleRates));
+            options.blockSizes = juce::VariantConverter<std::vector<int>>::fromVar (v.getProperty (IDs::blockSizes));
 
             for (auto c : v)
             {
@@ -674,8 +674,8 @@ private:
         v.setProperty (IDs::outputDir, options.outputDir.getFullPathName(), nullptr);
         v.setProperty (IDs::withGUI, options.withGUI, nullptr);
         v.setProperty (IDs::disabledTests, options.disabledTests.joinIntoString ("\n"), nullptr);
-        v.setProperty (IDs::sampleRates, juce::VariantConverter<std::vector<double>>::toVar(options.sampleRates), nullptr);
-        v.setProperty (IDs::blockSizes, juce::VariantConverter<std::vector<int>>::toVar(options.blockSizes), nullptr);
+        v.setProperty (IDs::sampleRates, juce::VariantConverter<std::vector<double>>::toVar (options.sampleRates), nullptr);
+        v.setProperty (IDs::blockSizes, juce::VariantConverter<std::vector<int>>::toVar (options.blockSizes), nullptr);
 
         return v;
     }
