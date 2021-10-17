079a6737b97350383a66136855146effe79d2ecc
Maksims Radugins <maksims.radugins@sonarworks.com>
2021-10-17 22:21:38 +0300

    Modify report to correctly show up in GitLab

--- Source/JUnitReport.cpp
+++ Source/JUnitReport.cpp
@@ -1,56 +1,15 @@
 #include "JUnitReport.h"
 
-// JUnit example from https://www.ibm.com/docs/en/adfz/developer-for-zos/9.1.1?topic=formats-junit-xml-format
-//<?xml version="1.0" encoding="UTF-8" ?>
-//   <testsuites id="20140612_170519" name="New_configuration (14/06/12 17:05:19)" tests="225" failures="1262" time="0.001">
-//      <testsuite id="codereview.cobol.analysisProvider" name="COBOL Code Review" tests="45" failures="17" time="0.001">
-//         <testcase id="codereview.cobol.rules.ProgramIdRule" name="Use a program name that matches the source file name" time="0.001">
-//            <failure message="PROGRAM.cbl:2 Use a program name that matches the source file name" type="WARNING">
-//WARNING: Use a program name that matches the source file name
-//Category: COBOL Code Review – Naming Conventions
-//File: /project/PROGRAM.cbl
-//Line: 2
-//      </failure>
-//    </testcase>
-//  </testsuite>
-//</testsuites>
-
 namespace
 {
 
-String stringToId(const String& s)
-{
-    if (s.isEmpty())
-    {
-        return "-";
-    }
-
-    auto firstChar = s[0];
-    bool validFirstChar =
-            (firstChar >= 'a' && firstChar <= 'z') ||
-            (firstChar >= 'A' && firstChar <= 'Z');
-
-    String id = validFirstChar ? "" : "-";
-    for (auto it = s.begin(); it != s.end(); ++it)
-    {
-        auto c = *it;
-        bool valid =
-                (c >= 'a' && c <= 'z') ||
-                (c >= 'A' && c <= 'Z') ||
-                (c >= '0' && c <= '9') ||
-                c == '-' || c == '_' || c == ':' || c == '.';
-        id += valid ? c : '-';
-    }
-    return id.toLowerCase();
-}
-
-
-XmlElement* createTestCase(const UnitTestRunner::TestResult& r)
+XmlElement* createTestCase(const String& pluginName, const UnitTestRunner::TestResult& r)
 {
     auto testcase = new XmlElement("testcase");
 
-    //testcase->setAttribute("id", stringToId(r.unitTestName + "." + r.subcategoryName));
     testcase->setAttribute("name", r.subcategoryName);
+    testcase->setAttribute("classname", r.unitTestName);
+    testcase->setAttribute("file", pluginName);
 
     auto duration = (r.endTime - r.startTime).inMilliseconds();
     testcase->setAttribute("time", duration / 1000.0);
@@ -72,9 +31,9 @@ bool JUnitReport::write(const HashMap<String, Array<UnitTestRunner::TestResult>>
 {
     XmlElement testsuites("testsuites");
 
-    int gfailures = 0;
-    int64 gtime = 0;
-    int gcount = 0;
+    int total_failures = 0;
+    int64 total_time = 0;
+    int total_count = 0;
     for (auto it = allResults.begin(); it != allResults.end(); ++it)
     {
         const auto& results = it.getValue();
@@ -89,7 +48,7 @@ bool JUnitReport::write(const HashMap<String, Array<UnitTestRunner::TestResult>>
 
         for (const auto& r: results)
         {
-            auto testcase = createTestCase(r);
+            auto testcase = createTestCase(pluginName, r);
             // add test case to test suite
             testsuite->prependChildElement(testcase);
 
@@ -102,25 +61,24 @@ bool JUnitReport::write(const HashMap<String, Array<UnitTestRunner::TestResult>>
             count++;
         }
 
-        //testsuite->setAttribute("id", stringToId("pluginval." + SystemStats::getOperatingSystemName() + "." + pluginName));
+        testsuite->setAttribute("package", "pluginval");
         testsuite->setAttribute("name", "pluginval of " + pluginName + " on " + SystemStats::getOperatingSystemName());
         testsuite->setAttribute("tests", count);
         testsuite->setAttribute("failures", failures);
         testsuite->setAttribute("time", time / 1000.0);
 
-        // accumulate totals for all test suites
-        gfailures += failures;
-        gtime += time;
-        gcount += count;
-
         testsuites.prependChildElement(testsuite);
+
+        // accumulate totals for all test suites
+        total_failures += failures;
+        total_time += time;
+        total_count += count;
     }
 
-    testsuites.setAttribute("id", "plugins");
-    testsuites.setAttribute("name", "Plugin validations");
-    testsuites.setAttribute("tests", gcount);
-    testsuites.setAttribute("failures", gfailures);
-    testsuites.setAttribute("time", gtime / 1000.0);
+    testsuites.setAttribute("name", "pluginval test suites");
+    testsuites.setAttribute("tests", total_count);
+    testsuites.setAttribute("failures", total_failures);
+    testsuites.setAttribute("time", total_time / 1000.0);
 
     return testsuites.writeTo(output);
 }
