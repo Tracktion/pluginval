16a345e0c17f8bc3d8df9cc01e1f9f6830b14452
Joseph Lyncheski <directmusic94@gmail.com>
2025-10-20 05:50:18 -0500

    Add test to ensure the locale does not change (#155)

---
+++ Source/tests/LocaleTest.cpp
@@ -0,0 +1,54 @@
+/*==============================================================================
+
+  Copyright 2018 by Tracktion Corporation.
+  For more information visit www.tracktion.com
+
+   You may also use this code under the terms of the GPL v3 (see
+   www.gnu.org/licenses).
+
+   pluginval IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
+   EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
+   DISCLAIMED.
+
+ ==============================================================================*/
+
+#include "../PluginTests.h"
+#include "../TestUtilities.h"
+
+//==============================================================================
+struct LocaleTest   : public PluginTest
+{
+    LocaleTest()
+        : PluginTest ("Ensuring that the locale does not change during execution", 1)
+    {
+        startupLocale = std::setlocale(LC_ALL, nullptr);
+    }
+
+    void runTest (PluginTests& ut, juce::AudioPluginInstance& instance) override
+    {
+        ut.logMessage (juce::String ("INFO: Startup Locale: [LOC]")
+                           .replace ("LOC",juce::String (startupLocale), false));
+
+        if (instance.hasEditor())
+        {
+            ScopedPluginDeinitialiser scopedPluginDeinitialiser (instance);
+
+            {
+                ut.logMessage ("Opening editor...");
+                ScopedEditorShower editor (instance);
+            }
+        }
+
+        std::string newLocale = std::setlocale(LC_ALL, nullptr);
+        ut.expectEquals (startupLocale, newLocale, "Plugin changed locale. This can cause unexpected behavior.");
+        ut.logMessage (juce::String ("INFO: Shutdown Locale: [LOC]")
+                           .replace ("LOC",juce::String (newLocale), false));
+
+    }
+
+private:
+    std::string startupLocale;
+};
+
+static LocaleTest localeTest;
+
